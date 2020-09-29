238c299e9372270c965c9c2935b4bb37ded7657d
shivam5555 <42959029+shivam5555@users.noreply.github.com>
2020-09-29 20:55:31 +0530

    Create print_once.c
    It just print once value , you are thinking why i am committing these simple code.
    
    Who made this github so why are you thinking for me :)

---
+++ print_once.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  inta[5],i;
+  printf("Enter the value you want to add : \n");
+  
+  for(i=0;i<5;i++)
+  {
+      printf("Enter any 5 values not float %d :-> ",i);
+      scanf("%d",&a[i]);
+   }
+   for(i=0;i<5;i++)
+      {
+        printf("First value only : %d\n",a[i]);
+        break;
+       }
+   for(i=0;i<5;i++)
+      {
+          printf("First value only : %d\n",a[i]);
+          break;
+        }
+   for(i=0;i<5;i++)
+        {
+          printf("Values you entered are %d. :->%d\n",i,a[i]);
+          }
+          return 0;
+   }
+  
